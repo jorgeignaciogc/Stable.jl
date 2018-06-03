243deac3a68a85a7c5a0c3227a7c3d7a120cfd89
Jorge I. González Cázares <39862053+jorgeignaciogc@users.noreply.github.com>
2018-06-03 07:00:31 +0100

    Add files via upload

---
+++ src/StableDistributions.jl
@@ -0,0 +1,39 @@
+__precompile__(true)
+
+module StableDistributions
+
+  using Distributions
+  using StatsBase
+  using LambertW
+  using FastGaussQuadrature
+
+  importall Distributions
+
+  export
+    # distribution types
+    Stable,
+    StablePositive,
+    StableUnilateral,
+    StableSupremum,
+
+    # methods
+    rand,
+    maximum,
+    minimum,
+    insupport,
+    pdf,
+    cdf,
+    mgf,
+    cf,
+    mellin,
+    mean,
+    params
+
+  # Source Files
+
+  include("stable.jl")
+  include("stablepositive.jl")
+  include("stableunilateral.jl")
+  include("stablesupremum.jl")
+
+end
